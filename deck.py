3a4911c3adfc48245cc8460f71a11ed5c27a1ce0
alanwz <alan_wzhang@yahoo.com>
2016-07-03 16:33:05 -0700

    Add files via upload

---
+++ deck.py
@@ -0,0 +1,48 @@
+class Deck(object):
+	"""
+	The entire set of cards that will be used in one game. Uses a standard deck of playing cards with jokers included. Up to 2 decks can be used, depending on number
+	of players.
+
+	Attributes:
+		size: size of deck, represented as an array
+	"""
+
+	def __init__(self, size):
+		"""
+		Return a Deck object with a certain *size*
+		"""
+		self.size = size
+		deck = []
+
+	def shuffle(deck):
+
+
+
+
+class Card(object):
+	"""
+	A card from a standard deck plus jokers.
+
+	Attributes:
+		suit: card suit
+		value: card value
+		is_joker: joker attribute
+	"""
+	is_joker = false
+
+	def __init__(self, suit, value):
+		"""
+		Return a Card object with *suit* and *value*
+		"""
+		self.suit = suit
+		self.value = value
+
+	def is_joker(card):
+		"""
+		Determines if a card is a joker.
+		"""
+		if (card.suit == 0)
+			is_joker = true
+
+
+
